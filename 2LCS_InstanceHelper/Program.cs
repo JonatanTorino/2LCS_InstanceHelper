2dd64eb935ab5c8d848b757dc1e21125e5886bd3
Jonatan Emmanuel Torino Candia <jonatan.torino@hotmail.com>
2021-01-11 14:03:36 -0300

    Add project files.

---
+++ 2LCS_InstanceHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2LCS_InstanceHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
